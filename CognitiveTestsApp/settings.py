631dac9ac322d8bcd9b00da68679800ded9aff8d
ArvidsD <86151469+ArvidsD@users.noreply.github.com>
2024-05-07 15:45:42 +0300

    added settings and requirments

--- CognitiveTestsApp/settings.py
+++ CognitiveTestsApp/settings.py
@@ -36,8 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 EXPORT_FORMATS = [XLSX]
 
@@ -58,8 +57,10 @@ INSTALLED_APPS = [
     "corsheaders",
     "sslserver",
     "import_export",
-
+    "daphne",
 ]
+
+ASGI_APPLICATION = 'CognitiveTestsApp.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
